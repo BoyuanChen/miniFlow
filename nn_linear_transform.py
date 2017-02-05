52019572dfbb10f9ae950937dbb440706905fc9b
Boyuan Chen <bc2699@columbia.edu>
2017-02-05 17:31:48 -0500

    add functions for linear transform

---
+++ nn_linear_transform.py
@@ -0,0 +1,24 @@
+
+
+import numpy as np
+from miniflow_linear_transform import *
+
+X, W, b = Input(), Input(), Input()
+
+f = Linear(X, W, b)
+
+X_ = np.array([[-1., -2.], [-1, -2]])
+W_ = np.array([[2., -3], [2., -3]])
+b_ = np.array([-3., -5])
+
+feed_dict = {X: X_, W: W_, b: b_}
+
+graph = topological_sort(feed_dict)
+output = forward_pass(f, graph)
+
+"""
+Output should be:
+[[-9., 4.],
+[-9., 4.]]
+"""
+print(output)
